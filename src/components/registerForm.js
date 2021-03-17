7942bbc6a913f6244bf9fabb5f35e9b6b59ec3b6
Pedro Fraca <pedro.fraca@gmail.com>
2021-03-17 23:13:04 +0100

    Adding dialog

--- src/components/registerForm.js
+++ src/components/registerForm.js
@@ -3,6 +3,11 @@ import { useFormik } from 'formik';
 import * as yup from 'yup';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions';
 import Paper from '@material-ui/core/Paper';
 import * as request from 'superagent';
 import { makeStyles } from '@material-ui/core/styles';
@@ -29,7 +34,15 @@ const validationSchema = yup.object({
 
 const WithMaterialUI = () => {
 
+  const [open, setOpen] = React.useState(false);
+  const [selectedValue, setValue] = React.useState("");
+  const [dialogTitle, setDialogTitle] = React.useState("");
+
   const classes = useStyles();
+   
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const formik = useFormik({
     initialValues: {
@@ -42,13 +55,39 @@ const WithMaterialUI = () => {
         .send({ email: values.email })
         .set('Accept', 'application/json')
         .then(res => {
-          alert('yay got ' + JSON.stringify(res.body));
-        });
+            setDialogTitle("Email sent");
+            setValue(`We've send an email to ${formik.values.email}`);
+            setOpen(true);
+
+        }).catch(() => {
+            setDialogTitle("Error");
+            setValue(`Error sending email to ${formik.values.email}`);
+            setOpen(true);}
+        );
     },
   });
 
   return (
     <div className={classes.root}>
+    <Dialog
+        open={open}
+        selectedValue={selectedValue}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{dialogTitle}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {selectedValue}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
       <Paper elevation={0}> 
       In order to get an API key to access to the TOUR Api you've to verify your email, and then you'll get the api key.
       </Paper>
